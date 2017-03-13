ec359c9a849b1c84d11bb8e794155009c55b109c
TonyUCLA <TonyUCLA>
2017-03-13 14:28:58 -0700

    map component created

---
+++ src/components/Map.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+// importing sub-modules from the google maps react package
+import { GoogleMapLoader, GoogleMap, Marker } from 'react-google-maps'
+
+class Map extends React.Component {
+    render() {
+        return {
+            <div className="google-map"> Map Component is here! </div>
+        }
+    }
+}
+
+export default Map
